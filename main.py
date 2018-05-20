8da677a53eda0f4ad4b0cae23bb9e10db0a10f81
Yichen Wang <yichew14@uci.edu>
2018-05-19 17:56:14 -0700

    Revert "main now searches for 1 term"
    This reverts commit ffb88cbe7118b6cba1137184a889c4d984e20d14.

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 import search
-import sys #used for input
 
 #main function
 def main():
-    #load from database
-    searchMap = search.loadDict("dictionary.json")
-
-    #prompt user for input
-    searchInput = raw_input("Gicsggle Search: ")
-
-    #for a in searchMap:
-    #    print "%s , %s" % (a, searchMap[a])
-
-    #search for term
-    search.searchDict(searchInput, searchMap)
+    search.loadDict('dictionary.json')
 
 #main check
-if __name__ == "__main__":
-   main()
+if __name__ == '__main__':
+    main()
 
 
 
