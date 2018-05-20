ffb88cbe7118b6cba1137184a889c4d984e20d14
Desmen Hartono <desmen.hartono@gmail.com>
2018-05-19 17:16:15 -0700

    main now searches for 1 term
    add 2 search functions to search.py:
    dict loadDict(dictionary path)
    void searchDict(term, dict)

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import search
+import sys #used for input
 
 #main function
 def main():
-    search.loadDict('dictionary.json')
+    #load from database
+    searchMap = search.loadDict("dictionary.json")
+
+    #prompt user for input
+    searchInput = raw_input("Gicsggle Search: ")
+
+    #for a in searchMap:
+    #    print "%s , %s" % (a, searchMap[a])
+
+    #search for term
+    search.searchDict(searchInput, searchMap)
 
 #main check
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+   main()
 
 
 
