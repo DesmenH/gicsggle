8a31cc728fc414ac077c996e890f80bb56a59933
Yichen Wang <yichew14@uci.edu>
2018-05-19 18:10:53 -0700

    Revert "Revert "main now searches for 1 term""
    This reverts commit 8da677a53eda0f4ad4b0cae23bb9e10db0a10f81.

--- search.py
+++ search.py
@@ -1,18 +1,27 @@
 import json
+from collections import OrderedDict #used to import orderedDict
 
 #input: json file with dictionary
 #output: dictionary of searchMap
 def loadDict(inputDictPath):
     with open(inputDictPath, 'r') as dict:
-        searchMap = json.load(dict)
-        print "searchMap is a data type of: %s " % (type(searchMap))
+        searchMap = json.load(dict, object_pairs_hook = OrderedDict)
         dict.close()
         return searchMap
 
-#def searchDict(term, dict):
-
-
-searchMap = loadDict('dictionary.json')
+#input: term and dictionary to search term in
+#output: found: value of term in dict if found
+#    not found: Your search - %s - did not match any documents...
+def searchDict(term, dict):
+    if term in dict:
+        print dict[term]
+    else:
+        print "Your search - %s - did not match any documents." % term
+        print "\n Suggestions: \n"
+        print "  - Make sure all words are spelled correctly."
+        print "  - Try different keywords."
+        print "  - Try more general keywords."
+        print "  - Try fewer keywords."
     
     
 
